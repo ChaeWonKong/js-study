eca7b661fc923d9aed49213297eeb0e3112c3b01
공채원 <chaewonkong@gmail.com>
2018-06-28 14:10:07 -0700

    Add 'addList' function

--- todolist/todo.js
+++ todolist/todo.js
@@ -1,1 +1,8 @@
 // To Do Javascript
+
+
+function addList() {
+    const innerBox = '<input class="chk" type="checkbox"><input class="txt" type="text"></input></input>';
+    let box = document.getElementById('input');
+    box.innerHTML += '<br><br>' + innerBox;
+} 
