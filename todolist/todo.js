2f2e491a5188fb8b4f4d3287229076fc16debf3d
공채원 <chaewonkong@gmail.com>
2018-06-28 14:37:11 -0700

    Add 'submit' function

--- todolist/todo.js
+++ todolist/todo.js
@@ -5,4 +5,14 @@ function addList() {
     const innerBox = '<input class="chk" type="checkbox"><input class="txt" type="text"></input></input>';
     let box = document.getElementById('input');
     box.innerHTML += '<br><br>' + innerBox;
-} 
+}
+
+function submit() {
+    const txtInputs = document.getElementsByClassName('txt');
+    let box = document.getElementById('input');
+    let result = ""
+    for (let i=0; i < txtInputs.length; i++) {
+        result += '<input type="checkbox"><span>' + txtInputs[i].value + '</span></input><br><br>'
+    };
+    box.innerHTML = result;
+}
