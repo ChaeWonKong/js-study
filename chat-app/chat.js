87959f55723ad7fb76f9e2f28f2bc9c774d4e969
공채원 <chaewonkong@gmail.com>
2018-06-28 18:44:25 -0700

    Add getUser function to prompt user nickname

--- chat-app/chat.js
+++ chat-app/chat.js
@@ -1,11 +1,21 @@
 // WebChat Javascript
-function send() {
-    let content = document.getElementById('cmt');
-    if (content.value === "") {
-        alert("Please Enter any Comment")
-    } else {
-        let con = document.getElementById('con');
-        con.innerHTML += '<div class="conA"><div class="user">Leon</div><div class="txt"><p>' + content.value + '</p></div></div>';
-        content.value = "";
+
+var main = {
+    'user': "",
+    'getUser': function() {
+        this.user = prompt("Enter your nickname")
+        document.write(this.user);
+        
+    },
+    'send': function() {
+        let content = document.getElementById('cmt');
+        if (content.value === "") {
+            alert("Please Enter any Comment")
+        } else {
+            let con = document.getElementById('con');
+            con.innerHTML += '<div class="conA"><div class="user">'+ this.user 
+            +'</div><div class="txt"><p>' + content.value + '</p></div></div>';
+            content.value = "";
+        }
     }
 }
