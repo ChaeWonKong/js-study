7492cb7cd2b0ea3930fefcebea83674efd0d39a3
공채원 <chaewonkong@gmail.com>
2018-06-28 18:32:30 -0700

    Add submit function

--- chat-app/chat.js
+++ chat-app/chat.js
@@ -1,1 +1,11 @@
 // WebChat Javascript
+function send() {
+    let content = document.getElementById('cmt');
+    if (content.value === "") {
+        alert("Please Enter any Comment")
+    } else {
+        let con = document.getElementById('con');
+        con.innerHTML += '<div class="conA"><div class="user">Leon</div><div class="txt"><p>' + content.value + '</p></div></div>';
+        content.value = "";
+    }
+}
